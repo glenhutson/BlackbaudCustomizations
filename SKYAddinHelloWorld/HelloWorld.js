e725a1597af1764475d50d958402fe441045272b
Glen Hutson <85623536+glenhutson@users.noreply.github.com>
2025-10-27 15:14:33 -0400

    Updated Aux asset references in HelloWorld.js

--- SKYAddinHelloWorld/HelloWorld.js
+++ SKYAddinHelloWorld/HelloWorld.js
@@ -138,7 +138,7 @@
         var context = $.extend(true, {}, initialArgs.context);
         context['anotherValue'] = 'this value was passed to the modal';
 
-        showModal('https://blackbaudaddinhelloworld.azurewebsites.net/helloworldmodal.html', context);
+        showModal('https://glenhutson.github.io/BlackbaudCustomizations/SKYAddinHelloWorld/HelloWorldModal.html', context);
     }
 
     function showAddCustomerModal() {
@@ -171,7 +171,7 @@
 
         // show a flyout and pass it some content
         client.showFlyout({
-            url: 'https://blackbaudaddinhelloworld.azurewebsites.net/helloworldflyout.html',
+            url: 'https://glenhutson.github.io/BlackbaudCustomizations/SKYAddinHelloWorld/HelloWorldFlyout.html',
             showIterator: true,
             iteratorPreviousDisabled: false,
             iteratorNextDisabled: false,
